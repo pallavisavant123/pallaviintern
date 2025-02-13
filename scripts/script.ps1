9df72eae74a0141f980b0779d362d5e2bb3aa982
pallavisavant123 <pallavi.savant@ccsheriff.org>
2025-02-13 09:24:39 -0600

    test22222

--- scripts/script.ps1
+++ scripts/script.ps1
@@ -10,39 +10,39 @@ $networkPath = "\\ITU-IISDEV-D02\Websites\git-secrets-master"
 $repoPath = Get-Location
 
 
-if (-Not $installed){
+# if (-Not $installed){
 
-    if (Test-Path $networkPath){
-        Copy-Item -Path $networkPath -Destination $repoPath -Recurse -Force
-        cd .\git-secrets-master
-        Start-Process -FilePath "C:\Windows\System32\WindowsPowershell\v1.0\powershell.exe" -ArgumentList ".\install.ps1" -Wait -NoNewWindow
-        cd ..
-        Remove-Item -Recurse -Force "git-secrets-master"
-    }
-    # else{
-    #     git config --global http.sslBackend schannel
+#     if (Test-Path $networkPath){
+#         Copy-Item -Path $networkPath -Destination $repoPath -Recurse -Force
+#         cd .\git-secrets-master
+#         Start-Process -FilePath "C:\Windows\System32\WindowsPowershell\v1.0\powershell.exe" -ArgumentList ".\install.ps1" -Wait -NoNewWindow
+#         cd ..
+#         Remove-Item -Recurse -Force "git-secrets-master"
+#     }
+#     # else{
+#     #     git config --global http.sslBackend schannel
 
-    #     git clone https://github.com/awslabs/git-secrets.git "git-secrets-master"
-    #     cd .\git-secrets-masters
-    #     Start-Process -FilePath "C:\Windows\System32\WindowsPowershell\v1.0\powershell.exe" -ArgumentList ".\install.ps1" -Wait -NoNewWindow
-    #     cd ..
-    #     Remove-Item -Recurse -Force "git-secrets-master"
-
-
-    # }
-
-    $gitSecretOutput = git secrets 2>&1 | Select-Object -First 1
+#     #     git clone https://github.com/awslabs/git-secrets.git "git-secrets-master"
+#     #     cd .\git-secrets-masters
+#     #     Start-Process -FilePath "C:\Windows\System32\WindowsPowershell\v1.0\powershell.exe" -ArgumentList ".\install.ps1" -Wait -NoNewWindow
+#     #     cd ..
+#     #     Remove-Item -Recurse -Force "git-secrets-master"
 
 
-    $installed = $gitSecretOutput -match "^usage"
+#     # }
+
+#     $gitSecretOutput = git secrets 2>&1 | Select-Object -First 1
 
 
-    if (-Not $installed){
-        Write-Host "git-secrets installation failed."
-        exit 1
-    }
-}
+#     $installed = $gitSecretOutput -match "^usage"
 
-git secrets --install
 
-Write-Host "Completed"
+#     if (-Not $installed){
+#         Write-Host "git-secrets installation failed."
+#         exit 1
+#     }
+# }
+
+# git secrets --install
+
+# Write-Host "Completed"
