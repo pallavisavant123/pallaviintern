b5bd64a2dd086d211b4c2e3540c789e573c62c29
pallavisavant123 <pallavi.savant@ccsheriff.org>
2025-02-13 09:38:24 -0600

    test777

--- scripts/script.ps1
+++ scripts/script.ps1
@@ -23,21 +23,23 @@ if (-Not $installed){
 #         Remove-Item -Recurse -Force "git-secrets-master"
     # }
 #     # else{
-#     #     git config --global http.sslBackend schannel
+        git config --global http.sslBackend schannel
 
         git clone https://github.com/awslabs/git-secrets.git "git-secrets-master"
         cd .\git-secrets-master
         Start-Process -FilePath "C:\Windows\System32\WindowsPowershell\v1.0\powershell.exe" -ArgumentList ".\install.ps1" -Wait -NoNewWindow
-#     #     cd ..
-#     #     Remove-Item -Recurse -Force "git-secrets-master"
+        cd ..
+        Remove-Item -Recurse -Force "git-secrets-master"
 
 
 #     # }
 
-#     $gitSecretOutput = git secrets 2>&1 | Select-Object -First 1
+    $gitSecretOutput = git secrets 2>&1 | Select-Object -First 1
 
 
-#     $installed = $gitSecretOutput -match "^usage"
+    $installed = $gitSecretOutput -match "^usage"
+
+    Write-Host $installed
 
 
 #     if (-Not $installed){
