d20509ed3a43fb8cd7547883446dd68bac36044e
pallavisavant123 <pallavi.savant@ccsheriff.org>
2025-02-13 09:30:55 -0600

    test3333

--- scripts/script.ps1
+++ scripts/script.ps1
@@ -9,17 +9,21 @@ Write-Host $installed
 $networkPath = "\\ITU-IISDEV-D02\Websites\git-secrets-master"
 $repoPath = Get-Location
 
+$test = Test-Path $networkPath
+
+Write-Host $test
+
 
 if (-Not $installed){
 
-    if (Test-Path $networkPath){
-        Write-Host "Not installed"
+    # if (Test-Path $networkPath){
+    #     Write-Host "Not installed"
 #         Copy-Item -Path $networkPath -Destination $repoPath -Recurse -Force
 #         cd .\git-secrets-master
 #         Start-Process -FilePath "C:\Windows\System32\WindowsPowershell\v1.0\powershell.exe" -ArgumentList ".\install.ps1" -Wait -NoNewWindow
 #         cd ..
 #         Remove-Item -Recurse -Force "git-secrets-master"
-    }
+    # }
 #     # else{
 #     #     git config --global http.sslBackend schannel
 
