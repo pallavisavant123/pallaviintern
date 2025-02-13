bfe7014d3fbc1e9e26c1aa4e11d5bc34d9262570
pallavisavant123 <pallavi.savant@ccsheriff.org>
2025-02-13 09:53:40 -0600

    test11

--- scripts/script.ps1
+++ scripts/script.ps1
@@ -28,8 +28,10 @@ if (-Not $installed){
         git clone https://github.com/awslabs/git-secrets.git "git-secrets-master"
         Write-Host $repoPath
         cd .\git-secrets-master
+        Write-Host $repoPath
         Start-Process -FilePath "C:\Windows\System32\WindowsPowershell\v1.0\powershell.exe" -ArgumentList ".\install.ps1" -Wait -NoNewWindow
         cd ..
+        Write-Host $repoPath
         Remove-Item -Recurse -Force "git-secrets-master"
 
 
