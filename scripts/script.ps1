94d0f4e49cce477f7a2286de9f1951693a3dec50
pallavisavant123 <pallavi.savant@ccsheriff.org>
2025-02-13 08:14:38 -0600

    Added new logic

---
+++ scripts/script.ps1
@@ -0,0 +1,48 @@
+# Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+$gitSecretOutput = git secrets 2>&1 | Select-Object -First 1
+
+$installed = $gitSecretOutput -match "^usage"
+
+Write-Host $installed
+
+$networkPath = "\\ITU-IISDEV-D02\Websites\git-secrets-master"
+$repoPath = Get-Location
+
+
+if (-Not $installed){
+
+    if (Test-Path $networkPath){
+        Copy-Item -Path $networkPath -Destination $repoPath -Recurse -Force
+        cd .\git-secrets-master
+        Start-Process -FilePath "C:\Windows\System32\WindowsPowershell\v1.0\powershell.exe" -ArgumentList ".\install.ps1" -Wait -NoNewWindow
+        cd ..
+        Remove-Item -Recurse -Force "git-secrets-master"
+    }
+    else{
+        git config --global http.sslBackend schannel
+
+        git clone https://github.com/awslabs/git-secrets.git "git-secrets-master"
+        cd .\git-secrets-masters
+        Start-Process -FilePath "C:\Windows\System32\WindowsPowershell\v1.0\powershell.exe" -ArgumentList ".\install.ps1" -Wait -NoNewWindow
+        cd ..
+        Remove-Item -Recurse -Force "git-secrets-masters"
+
+
+    }
+
+    $gitSecretOutput = git secrets 2>&1 | Select-Object -First 1
+
+
+    $installed = $gitSecretOutput -match "^usage"
+
+
+    if (-Not $installed){
+        Write-Host "git-secrets installation failed."
+        exit 1
+    }
+}
+
+git secrets --install
+
+Write-Host "Completed"
